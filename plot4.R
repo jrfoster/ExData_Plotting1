14e3a8bb27af4a19458d78a41845d425a933cc7b
Jason Foster <jason.foster@orionhealth.com>
2015-10-11 13:14:08 -0700

    Correcting legend border

--- plot4.R
+++ plot4.R
@@ -49,7 +49,7 @@ third <- function(x) {
    plot(x$Timestamp, x$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")   
    lines(x$Timestamp, x$Sub_metering_2, col = "red")
    lines(x$Timestamp, x$Sub_metering_3, col = "blue")
-   legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1,1,1))
+   legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1,1,1), bty = "n")
 }
 
 fourth <- function(x) {
